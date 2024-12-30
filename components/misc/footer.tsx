e75c0c664d2cd9ba5527bc83e7e16a6ed3b54bcc
DennisVanBetsUF <dennis.vanbets@unifly.aero>
2024-12-30 17:50:14 +0100

    adapt template

--- components/misc/footer.tsx
+++ components/misc/footer.tsx
@@ -6,21 +6,18 @@ const Footer = () => {
       <div className="container mx-auto px-5">
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            <PreviewLink href="/statically-generated">Statically Generated</PreviewLink> with Next.js.
+           Copyright Dennis Van Bets 2024
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
               href="#"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
-              Read Documentation
+              Home
             </a>
-            <a
-              href={`https://github.com/matthewwong525/linked-blog-starter`}
-              className="mx-3 font-bold hover:underline"
-            >
-              View on GitHub
-            </a>
+            <div className="mx-3 font-bold">
+              Mindmap
+            </div>
           </div>
         </div>
       </div>
