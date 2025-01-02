d10741721a893e4cd845a6fa4cbfc61270efa1ef
DennisVanBetsUF <dennis.vanbets@unifly.aero>
2025-01-03 00:16:21 +0100

    import callouts css from package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app'
 import '../styles/index.css'
+import "rehype-obsidian-callout/styles/styles.css";
 
 
 export default function MyApp({ Component, pageProps }: AppProps) {
