c6fa395139eb800934be53f1ed5afe21195061a0
DennisVanBetsUF <dennis.vanbets@unifly.aero>
2025-01-03 00:33:11 +0100

    remove dumb stuff. move vars to module

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,11 @@
 import { AppProps } from 'next/app'
 import '../styles/index.css'
-import "rehype-obsidian-callout/styles/styles.css";
 
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-    const style = `
-    :root {
-        --callout-note-rgb: 0, 176, 255;
-        --callout-success-rgb: 0, 200, 83;
-        --callout-warning-rgb: 255, 145, 0;
-        --callout-error-rgb: 255, 0, 0;
-        --callout-quote-rgb: 158, 158, 158;
-    }
-    `
-
   return (
     <>
-      <Component {...pageProps} style={style}/>
+      <Component {...pageProps}/>
     </>
   )
 }
