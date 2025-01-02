d076f3db49b703b8a62043aed3b1b52ca7b2f9da
DennisVanBetsUF <dennis.vanbets@unifly.aero>
2025-01-03 00:21:16 +0100

    attempt dumb stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,19 @@ import "rehype-obsidian-callout/styles/styles.css";
 
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+    const style = `
+    :root {
+        --callout-note-rgb: 0, 176, 255;
+        --callout-success-rgb: 0, 200, 83;
+        --callout-warning-rgb: 255, 145, 0;
+        --callout-error-rgb: 255, 0, 0;
+        --callout-quote-rgb: 158, 158, 158;
+    }
+    `
+
   return (
     <>
-      <Component {...pageProps} />
+      <Component {...pageProps} style={style}/>
     </>
   )
 }
