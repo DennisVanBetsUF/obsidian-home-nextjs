b99b5038b2ad6cd9c4fe06a5f60208b3313260b9
DennisVanBetsUF <dennis.vanbets@unifly.aero>
2025-01-03 22:30:18 +0100

    this works?

--- common_md/home.md
+++ common_md/home.md
@@ -4,6 +4,8 @@ excerpt: Obsidian Mindmap for Home Renovation
 ---
 ![](attachment/3dd8096a1d6b54b748a70a74ccf9cabf.jpg)
 
+> [!note] allow
+
 # [[Glossary|Woordenboek]]
 # [[Aankoop]]
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
 import { AppProps } from 'next/app'
-import '../styles/index.css'
+import '../styles/globals.css'
 
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps}/>
+        <div className="callout-type-info callout-block">
+            <div className="callout-title-section"><p
+                className="callout-title">Info</p></div>
+            <div className="callout-content-section"><p>hello callout</p></div>
+        </div>
     </>
   )
 }
