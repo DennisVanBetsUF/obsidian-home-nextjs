ed8f341ac625090cedf62211b903f4aeede3a297
DennisVanBetsUF <dennis.vanbets@unifly.aero>
2025-01-03 23:02:38 +0100

    add custom tip callout

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -12,7 +12,15 @@ import {Element} from 'hast-util-select'
 import { renderToStaticMarkup } from "react-dom/server"
 import NotePreview from '../components/misc/note-preview'
 import { fromHtml } from 'hast-util-from-html'
-import plugin from "rehype-obsidian-callout";
+import calloutsPlugin from "rehype-obsidian-callout";
+
+const calloutsConfig = {
+  callouts: {
+    tip: {
+      icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="rgb(83, 223, 221)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M8.5 14.5A2.5 2.5 0 0 0 11 12c0-1.38-.5-2-1-3-1.072-2.143-.224-4.054 2-6 .5 2.5 2 4.9 4 6.5 2 1.6 3 3.5 3 5.5a7 7 0 1 1-14 0c0-1.153.433-2.294 1-3a2.5 2.5 0 0 0 2.5 2.5z"></path></svg>`
+    }
+  }
+};
 
 
 export async function markdownToHtml(markdown: string, currSlug: string) {
@@ -31,7 +39,7 @@ export async function markdownToHtml(markdown: string, currSlug: string) {
     .use(remarkParse)
     .use(remarkGfm)
     .use(remarkRehype)
-    .use(plugin)
+    .use(calloutsPlugin, calloutsConfig)
     // .use(rehypeSanitize)
     .use(rehypeRewrite, {
       selector: 'a',
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,11 +6,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps}/>
-        <div className="callout-type-info callout-block">
-            <div className="callout-title-section"><p
-                className="callout-title">Info</p></div>
-            <div className="callout-content-section"><p>hello callout</p></div>
-        </div>
     </>
   )
 }
